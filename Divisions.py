089de9c8c5612c49d6cf9af9f2bb1a00c88ba4ac
CJW8392 <113397376+CJW8392@users.noreply.github.com>
2022-09-13 21:12:29 -0400

    Add files via upload
    Simple divisions and odd or even determiner.

---
+++ Divisions.py
@@ -0,0 +1,9 @@
+num = int(input("Pick a number, any number!"))
+
+if(num%4 == 0):
+    print("Your number is divisble by 4")
+elif(num%2 == 0):
+    print("Your number is divisible by 2")
+else:
+    print("You have selected an odd number")
+    
